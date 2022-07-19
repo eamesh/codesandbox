149745ecb6d4c815a764711b8d3ec72302637524
eamesh <easeava@gmail.com>
2022-07-20 00:15:23 +0800

    feat: update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd setup
 docker build -t codesandbox/build .
 
-cd ../
-docker run -it --rm -v ./build:/app codesandbox/build bash -c cd /workspace && cp -rf ./www/static/* ./packages/app/www/static && cp -rf /workspace /build
+cd ../ && rm -rf ./app && mkdir ./app
+docker run -it --rm -v app:/app codesandbox/build bash -c cd /workspace && cp -rf ./www/static/* ./packages/app/www/static && cp -rf /workspace /app
 
 docker-compose up -d
